4cf96bfb5b378188d29edf49f48e520e26adc460
diegosorace <dsorace@prismamp.com>
2023-01-24 23:09:29 -0300

    update

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 #------------------------------------------------------------------------------
 # AWS ECS Task Execution Role
 #------------------------------------------------------------------------------
-output "aws_iam_role_ecs_task_execution_role_arn" {
-  description = "The Amazon Resource Name (ARN) specifying the role."
-  value       = aws_iam_role.ecs_task_execution_role.arn
-}
-output "aws_iam_role_ecs_task_execution_role_create_date" {
-  description = "The creation date of the IAM role."
-  value       = aws_iam_role.ecs_task_execution_role.create_date
-}
-output "aws_iam_role_ecs_task_execution_role_description" {
-  description = "The description of the role."
-  value       = aws_iam_role.ecs_task_execution_role.description
-}
-output "aws_iam_role_ecs_task_execution_role_id" {
-  description = "The ID of the role."
-  value       = aws_iam_role.ecs_task_execution_role.id
-}
-output "aws_iam_role_ecs_task_execution_role_name" {
-  description = "The name of the role."
-  value       = aws_iam_role.ecs_task_execution_role.name
-}
-output "aws_iam_role_ecs_task_execution_role_unique_id" {
-  description = "The stable and unique string identifying the role."
-  value       = aws_iam_role.ecs_task_execution_role.unique_id
-}
+#output "aws_iam_role_ecs_task_execution_role_arn" {
+#  description = "The Amazon Resource Name (ARN) specifying the role."
+#  value       = aws_iam_role.ecs_task_execution_role.arn
+#}
+#output "aws_iam_role_ecs_task_execution_role_create_date" {
+#  description = "The creation date of the IAM role."
+#  value       = aws_iam_role.ecs_task_execution_role.create_date
+#}
+#output "aws_iam_role_ecs_task_execution_role_description" {
+#  description = "The description of the role."
+#  value       = aws_iam_role.ecs_task_execution_role.description
+#}
+#output "aws_iam_role_ecs_task_execution_role_id" {
+#  description = "The ID of the role."
+#  value       = aws_iam_role.ecs_task_execution_role.id
+#}
+#output "aws_iam_role_ecs_task_execution_role_name" {
+#  description = "The name of the role."
+#  value       = aws_iam_role.ecs_task_execution_role.name
+#}
+#output "aws_iam_role_ecs_task_execution_role_unique_id" {
+#  description = "The stable and unique string identifying the role."
+#  value       = aws_iam_role.ecs_task_execution_role.unique_id
+#}
 
 #------------------------------------------------------------------------------
 # ECS Task Definition
